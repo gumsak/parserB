506043fa0136309a5396392495deedcb7877c61b
gmsk <gumsak@users.noreply.github.com>
2017-07-10 23:01:12 +0200

    File translated form Fr to Eng

--- parser.c
+++ parser.c
@@ -8,80 +8,85 @@ int main(){
     int i=0;
 
     FILE* text = NULL;
-    text = fopen("descartes.txt", "r");//A modifier selon le nom du texte à traiter
+    text = fopen("descartes.txt", "r");//Replace "descartes.txt" with your text file (the source)
     FILE* dest = NULL;
-    dest = fopen("dest.xml","w+");//Indiquer le nom et le type de fichier qu'on veut générer
+    dest = fopen("dest.xml","w+");//Will create and write your text + the tags in the "dest.xml" file (the destination)
 
-//////////////CONNAITRE LE NBR DE CARACATERES DANS LA SOURCE - PAS UTILE/////////////
+	/* ---Get the number of characters in the given file--- */
     do{
     nbcar = fgetc(text);
      i+=1;
         } while (nbcar != EOF);
 
     rewind(text);
-    printf("Le texte original contient %d caracteres.\n",i );
-///////////////********************************************************/////////////
+    printf("The original text contains %d characters.\n",i );
+	/* ----------------------------------------------------- */
 
-    int car;
+    int car; /* //!!\\ INITIAlLY WRITTEN IN FRENCH --> the variable's name is 'car', short for 'caractere', 
+	which means character, feel free to change it and its occurrences */
     int tmp1, tmp2;
 
-    fprintf(dest,"<p>");//balise debut de texte
+    fprintf(dest,"<p>");//puts the given tag at the very start of our text
 
     do{
         car=fgetc(text);
 
-// Condition pour fin de phrase
+		/* Finds if it's the end of a sentence */
+        if(car==46 || car==33 || car==63){ //if character is '.' or '!' or '?'
 
-        if(car==46 || car==33 || car==63){ //si caractere == . || ! || ?
+            fprintf(dest,"%c",car);//write the character
+            tmp1=fgetc(text); //tmp1=character after car
 
-            fprintf(dest,"%c",car);//ecrire ce caractere
-            tmp1=fgetc(text); //tmp1=caractere present apres car
-
-            if(tmp1==32 || tmp1==13 || tmp1==10){ //si == ' ' || retour chariot || saut de ligne
-                fprintf(dest,"</p>");
-                fprintf(dest,"%c",tmp1);//ecrire le caractere
-                fprintf(dest,"<p>");//considerer que c'est la fin de phrase
+            if(tmp1==32 || tmp1==13 || tmp1==10){ //if == ' ' or Carriage return or Line Feed
+                fprintf(dest,"</p>");//we consider that this was a sentence, so we put a closing tag
+                fprintf(dest,"%c",tmp1);//write the char
+                fprintf(dest,"<p>");//put the opening tag for the next sentence
             }
-//Pour eviter d'ecrire le EOF
-            else if(tmp1==EOF){//on est a la fin du fichier
-
-               fprintf(dest,"</p>");//balise finale
+			
+			/* To avoid writting the EOF char */
+            else if(tmp1==EOF){//if we are at the end of the file
+			
+               fprintf(dest,"</p>");//write the final tag
             }
-//Si rien de specifique
-            else{//sinon on continu d'ecrire normalement la phrase
-
-                fprintf(dest,"%c",tmp1); //ecrire caractere dans dest
+			
+			/* If nothing of interest is read, then consider we're in 
+			the middle of a sentence, and just write the char as is */
+            else{
+                fprintf(dest,"%c",tmp1); 
             }
         }
-//Pour gerer les sauts de lignes/retours chariot
-        else if(car==13 || car==10){//si car == retour chariot || saut de ligne
+		
+		/* This takes care of Carriage return and Line Feed */
+        else if(car==13 || car==10){//if the read char is a Carriage return or a Line Feed
 
-            tmp2=fgetc(text); //tmp2=caractere present apres car
-
-            if(tmp2>=65 && tmp2<=90){//si MAJ a la ligne, alors fin de phrase
-                fprintf(dest,"</p>");
+            tmp2=fgetc(text); //tmp2 = the character that comes after the currently read char (i.e 'car')
+			
+			/* if the char in the next line is a capital letter, then the sentence is over */
+            if(tmp2>=65 && tmp2<=90){
+                fprintf(dest,"</p>")//so we put the end tag, etc...
                 fprintf(dest,"%c",car);
                 fprintf(dest,"<p>");
                 fprintf(dest,"%c",tmp2);
-
             }
 
-            else if(tmp2==13 || tmp2==10){//si encore retour chariot || saut de ligne, alors fin de phrase
+			/* else if the char in the next line is a Carriage return or a Line Feed again, then the sentence is over */
+            else if(tmp2==13 || tmp2==10){
                 fprintf(dest,"</p>");
                 fprintf(dest,"%c",car);
-
                 fprintf(dest,"%c",tmp2);
                 fprintf(dest,"<p>");
             }
 
-            else{//sinon si simple saut de ligne mais pas fin de phrase
+			/* Otherwise, it was a simple carrige/Line Feed in the middle of a sentence, so keep writting the text */
+            else{
                 fprintf(dest,"%c",car);
-                fprintf(dest,"%c",tmp2); //ecrire caractere dans dest
+                fprintf(dest,"%c",tmp2);
             }
         }
-
-        else{//sinon simplement ecrire le caractere
-            fprintf(dest,"%c",car); //ecrire caractere dans dest
+		
+		/* If none of the above apply, then just keep writing the characters */
+        else{
+            fprintf(dest,"%c",car);
         }
 
     }while(car!= EOF);
